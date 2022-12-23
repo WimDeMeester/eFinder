9bf3538a3ffce89d9cfe8a87006d24aee5e9cea4
Mike Rosseel <mike.rosseel@gmail.com>
2022-12-23 14:22:40 +0100

    some more fixes

--- src/gui.py
+++ src/gui.py
@@ -12,6 +12,7 @@ from efinder_core import EFinder
 from Nexus import Nexus
 import re
 import time
+import os
 
 
 class EFinderGUI():
@@ -236,13 +237,14 @@ class EFinderGUI():
         if self.mirror.get() == "1":
             img2 = ImageOps.mirror(img2)
         if self.auto_rotate.get() == "1":
-            img2 = img2.rotate(scopeAlt)
+            img2 = img2.rotate(self.astro_data.scopeAlt)
         elif self.manual_rotate.get() == "1":
             angle_deg = self.angle.get()
             img2 = img2.rotate(float(angle_deg))
-        img2 = ImageTk.PhotoImage(img2)
-        self.panel.configure(image=img2)
-        self.panel.image = img2
+        self.img3 = img2
+        self.img2 = ImageTk.PhotoImage(img2)
+        self.panel.configure(image=self.img2)
+        self.panel.image = self.img2
         self.panel.place(x=200, y=5, width=1014, height=760)
 
     # GUI specific
@@ -431,8 +433,8 @@ class EFinderGUI():
         ).pack(padx=1, pady=1)
 
         self.box_write(
-            "ccd is " + self.camera_data.camera.get_cam_type(), False)
-        self.box_write("Nexus " + NexStr, True)
+            "ccd is " + self.camera_data.camera.get_cam_type())
+        self.box_write("Nexus " + NexStr)
 
         but_frame = Frame(self.window, bg="black")
         but_frame.place(x=25, y=650)
@@ -895,13 +897,13 @@ class EFinderGUI():
         self.efinder.solveImage()
         self.image_show()
         if self.astro_data.solved:
-            self.box_write("no solution yet", True)
+            self.box_write("no solution yet")
             return
         goto_ra = self.nexus.get(":Gr#").split(":")
         goto_dec = re.split(r"[:*]", self.nexus.get(":Gd#"))
         # not a valid goto target set yet.
         if goto_ra[0] == "00" and goto_ra[1] == "00":
-            self.box_write("no GoTo target", True)
+            self.box_write("no GoTo target")
             return
         logging.info("%s %s %s" % ("goto RA & Dec", goto_ra, goto_dec))
         ra = float(goto_ra[0]) + float(goto_ra[1]) / 60 + float(goto_ra[2]) / 3600
@@ -916,11 +918,12 @@ class EFinderGUI():
         delta_Az_str = "{: .2f}".format(delta_Az)
         delta_Alt_str = "{: .2f}".format(delta_Alt)
         logging.info(f"deltaAz: {delta_Az_str}, deltaAlt: {delta_Alt_str}")
-        self.box_write(f"deltaAz : {delta_Az_str}", True)
-        self.box_write(f"deltaAlt: {delta_Alt_str}", True)
+        self.box_write(f"deltaAz : {delta_Az_str}")
+        self.box_write(f"deltaAlt: {delta_Alt_str}")
         self.moveScope(delta_Az, delta_Alt)
         # could insert a new capture and solve?
 
+    # UI only method
     def moveScope(self, dAz, dAlt):
         azPulse = abs(dAz / float(self.param["azSpeed"]))  # seconds
         altPulse = abs(dAlt / float(self.param["altSpeed"]))
@@ -929,7 +932,7 @@ class EFinderGUI():
                 "azPulse:", azPulse, "altPulse:", altPulse, "seconds")
         )
         self.nexus.write("#:RG#")  # set move speed to guide
-        self.box_write("moving scope in Az", True)
+        self.box_write("moving scope in Az")
         logging.info("moving scope in Az")
         if dAz > 0:  # if +ve move scope left
             self.nexus.write("#:Me#")
@@ -940,7 +943,7 @@ class EFinderGUI():
             time.sleep(azPulse)
             self.nexus.write("#:Q#")
         time.sleep(0.2)
-        self.box_write("moving scope in Alt", True)
+        self.box_write("moving scope in Alt")
         logging.info("moving scope in Alt")
         self.nexus.write("#:RG#")
         if dAlt > 0:  # if +ve move scope down
@@ -951,9 +954,96 @@ class EFinderGUI():
             self.nexus.write("#:Mn#")
             time.sleep(altPulse)
             self.nexus.write("#:Q#")
-        self.box_write("move finished", True)
+        self.box_write("move finished")
         logging.info("move finished")
         time.sleep(1)
+
+    def annotate_image(self):
+        # global img3, bright, hip, hd, abell, ngc, tycho2
+        scale_low = str(
+            self.camera_data.pix_scale * 0.9 * 1.2
+        )  # * 1.2 is because image has been resized for the display panel
+        scale_high = str(self.camera_data.pix_scale * 1.1 * 1.2)
+        self.image_show()
+        self.img3 = self.img3.save(self.cli_data.images_path / "adjusted.jpg")
+        # first need to re-solve the image as it is presented in the GUI, saved as 'adjusted.jpg'
+        annotate_cmd = (
+            "solve-field --no-plots --new-fits none --solved none --match none --corr none \
+                --rdls none --cpulimit 10 --temp-axy --overwrite --downsample 2 --no-remove-lines --uniformize 0 \
+                --scale-units arcsecperpix --scale-low "
+            + scale_low
+            + " \
+                --scale-high "
+            + scale_high
+            + " "
+            + str(self.cli_data.images_path / "adjusted.jpg")
+        )
+        logging.debug(f"Annotating image with cmd: {annotate_cmd}")
+        os.system(annotate_cmd)
+        # now we can annotate the image adjusted.jpg
+        opt1 = " " if self.bright.get() == "1" else " --no-bright"
+        opt2 = (
+            " --hipcat=/usr/local/astrometry/annotate_data/hip.fits --hiplabel"
+            if self.hip.get() == "1"
+            else " "
+        )
+        opt3 = (
+            " --hdcat=/usr/local/astrometry/annotate_data/hd.fits"
+            if self.hd.get() == "1"
+            else " "
+        )
+        opt4 = (
+            " --abellcat=/usr/local/astrometry/annotate_data/abell-all.fits"
+            if self.abell.get() == "1"
+            else " "
+        )
+        opt5 = (
+            " --tycho2cat=/usr/local/astrometry/annotate_data/tycho2.kd"
+            if self.tycho2.get() == "1"
+            else " "
+        )
+        opt6 = " " if self.ngc.get() == "1" else " --no-ngc"
+        try:  # try because the solve may have failed to produce adjusted.jpg
+            cmd = (
+                'python3 /usr/local/astrometry/lib/python/astrometry/plot/plotann.py \
+                    --no-grid --tcolor="orange" --tsize="14" --no-const'
+                + opt1
+                + opt2
+                + opt3
+                + opt4
+                + opt5
+                + opt6
+                + " "
+                + " ".join(
+                    [
+                        str(self.cli_data.images_path / "adjusted.wcs"),
+                        str(self.cli_data.images_path / "adjusted.jpg"),
+                        str(self.cli_data.images_path / "adjusted_out.jpg"),
+                    ]
+                )
+            )
+            logging.debug(f"plotann cmd: {cmd}")
+            os.system(cmd)
+        except:
+            logging.debug("Exception during plotann")
+            pass
+        if os.path.exists(self.cli_data.images_path / "adjusted_out.jpg") == True:
+            img3 = Image.open(self.cli_data.images_path / "adjusted_out.jpg")
+            filelist = glob.glob(str(self.cli_data.images_path / "adjusted*.*"))
+            for filePath in filelist:
+                try:
+                    os.remove(filePath)
+                except:
+                    logging.error("problem while deleting file :", filePath)
+            self.box_write("annotation successful")
+            img4 = ImageTk.PhotoImage(img3)
+            self.panel.configure(image=img4)
+            self.panel.image = img4
+            self.panel.place(x=200, y=5, width=1014, height=760)
+        else:
+            self.box_write("solve failure")
+            return
+
 
     def measure_offset(self):
         logging.debug("TODO measure_offset")
@@ -961,10 +1051,7 @@ class EFinderGUI():
     def readTarget(self):
         logging.debug("TODO readTarget")
 
-    def annotate_image(self):
-        logging.debug("TODO annotate_image")
-
-    def box_write(self, new_line, show_handpad):
+    def box_write(self, new_line):
         t = self.ts.now()
         for i in range(5, 0, -1):
             self.box_list[i] = self.box_list[i - 1]
@@ -975,15 +1062,14 @@ class EFinderGUI():
                 x=1050, y=980 - i * 16)
 
     def deltaCalcGUI(self):
-        global deltaAz, deltaAlt, solved_altaz
-        deltaAz, deltaAlt = self.common.deltaCalc(
-            nexus.get_altAz(), solved_altaz, nexus.get_scope_alt(), deltaAz, deltaAlt
-        )
-        deltaAzstr = "{: .1f}".format(float(deltaAz)).ljust(8)[:8]
-        deltaAltstr = "{: .1f}".format(float(deltaAlt)).ljust(8)[:8]
-        tk.Label(window, width=10, anchor="e", text=deltaAzstr, bg=b_g, fg=f_g).place(
-            x=315, y=870
-        )
-        tk.Label(window, width=10, anchor="e", text=deltaAltstr, bg=b_g, fg=f_g).place(
-            x=315, y=892
-        )
+        # global deltaAz, deltaAlt, solved_altaz
+        self.astro_data.deltaAz, self.astro_data.deltaAlt = self.common.deltaCalc(
+            self.nexus.get_altAz(), self.astro_data.solved_altaz,
+            self.nexus.get_scope_alt(), self.astro_data.deltaAz,
+            self.astro_data.deltaAlt)
+        deltaAzstr = "{: .1f}".format(float(self.astro_data.deltaAz)).ljust(8)[:8]
+        deltaAltstr = "{: .1f}".format(float(self.astro_data.deltaAlt)).ljust(8)[:8]
+        tk.Label(self.window, width=10, anchor="e", text=deltaAzstr,
+                 bg=self.b_g, fg=self.f_g).place(x=315, y=870)
+        tk.Label(self.window, width=10, anchor="e", text=deltaAltstr,
+                 bg=self.b_g, fg=self.f_g).place(x=315, y=892)
